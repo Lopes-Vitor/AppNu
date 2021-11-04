6cfdd4e403ac1a1141f650c49c391cd48f3e81b4
L-Mosca <88050952+L-Mosca@users.noreply.github.com>
2021-11-04 20:42:45 -0300

    add text watcher and change layout colors.

--- WaterApp/Waterapp/app/src/main/java/co/vitorcavalin/waterapp/MainActivity.java
+++ WaterApp/Waterapp/app/src/main/java/co/vitorcavalin/waterapp/MainActivity.java
@@ -2,12 +2,18 @@ package co.vitorcavalin.waterapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TimePicker;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -29,16 +35,71 @@ public class MainActivity extends AppCompatActivity {
         timePicker = findViewById(R.id.time_picker);
 
         timePicker.setIs24HourView(true);
+
+
+        editMinutes.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if (editMinutes.length() == 3)
+                    hideKeyboardFrom(MainActivity.this, editMinutes);
+            }
+        });
+
     }
-    public void notifyClick(View view){
-        String sinterval = editMinutes.getText().toString();
 
-        hour = timePicker.getCurrentHour();
-        minute = timePicker.getCurrentMinute();
-        interval = Integer.parseInt(sinterval);
 
-        Log.d("teste", "hora: " + hour + " minuto: " + minute + " intervalo: " + interval);
+    public void notifyClick(View view) {
+        try {
 
+            hideKeyboardFrom(this, btnnotify);
+
+            String sInterval = editMinutes.getText().toString();
+
+            hour = timePicker.getCurrentHour();
+            minute = timePicker.getCurrentMinute();
+            interval = Integer.parseInt(sInterval);
+
+            //interval = Integer.parseInt(editMinutes.getText().toString());
+            Toast.makeText(this, "hora: " + hour + " minuto: " + minute + " intervalo: " + interval, Toast.LENGTH_SHORT).show();
+            Log.d("teste", "hora: " + hour + " minuto: " + minute + " intervalo: " + interval);
+        } catch (Exception e) {
+            Toast.makeText(this, "Preenche o campo seu arrombado", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    /*@Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.btn_notify) {
+
+            try {
+                String sInterval = editMinutes.getText().toString();
+
+                hour = timePicker.getCurrentHour();
+                minute = timePicker.getCurrentMinute();
+
+                interval = Integer.parseInt(sInterval);
+                Toast.makeText(this, "hora: " + hour + " minuto: " + minute + " intervalo: " + interval, Toast.LENGTH_LONG).show();
+            } catch (Exception e) {
+                Toast.makeText(this, "Digita um numero animal", Toast.LENGTH_SHORT).show();
+            }
+
+        }
+    }*/
+
+    public static void hideKeyboardFrom(Context context, View view) {
+        InputMethodManager imm = (InputMethodManager) context.getSystemService(Activity.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
     }
 
 }
